b0e77299dfb3f634312d19cb1886030414fb5957
maks-prost <zendzin@list.ru>
2019-08-14 13:41:22 +0500

    start heroku

--- debug.py
+++ debug.py
@@ -1,22 +1,22 @@
 from vk_api_my import VkApi 
-from initial_scraper import get_html
-from bs4 import BeautifulSoup
-url = 'https://p-balaev.livejournal.com/'
-find_all = ('div', {'class':"entry-wrap js-emojis"})
-find = ('a',{'class':"subj-link"})
-find_date = ('abbr' ,{'class':"updated"})
-soup = BeautifulSoup(get_html(url),'lxml')
+# from initial_scraper import get_html
+# from bs4 import BeautifulSoup
+# url = 'https://p-balaev.livejournal.com/'
+# find_all = ('div', {'class':"entry-wrap js-emojis"})
+# find = ('a',{'class':"subj-link"})
+# find_date = ('abbr' ,{'class':"updated"})
+# soup = BeautifulSoup(get_html(url),'lxml')
 
-# token = '9a3e3c787c27cdcffb50046fb31b70c4dbb6e1b78dacb8a91d7e1a6e28d6041731d6918fb84822f54483d'
-# group_id = -165089751
-# vk = VkApi(token)
+token = '9a3e3c787c27cdcffb50046fb31b70c4dbb6e1b78dacb8a91d7e1a6e28d6041731d6918fb84822f54483d'
+group_id = -165089751
+vk = VkApi(token)
 
 # # res =  vk.method('execute',
 # #  	{'code':
 # #  	'return API.wall.get(%s).items@.text+ API.wall.get(%s).items@.text;'%{"owner_id":-165089751, "count":2,"offset":0}%{"owner_id":-165089751, "count":2, "offset":2}
 # #  	 })
 	
-# res = vk.method('execute',
-			# {'code':
-			# 'return API.wall.get({"owner_id":%d,"count":100,"filter":"postponed"}).items@.text;'%group_id})
+res = vk.method('execute',
+			{'code':
+			'return API.wall.get({"owner_id":%d,"count":10,"filter":"postponed,all"}).items@.text;'%group_id})
 		
--- load_post_vk_class.py
+++ load_post_vk_class.py
@@ -42,7 +42,10 @@ class Load_Post():
 		#gjследние 300 публикацмй на странице
 		self.list_saves = self.vk_user.method('execute',
 			{'code':
-			'return %s;'%'+'.join(map(lambda s: 'API.wall.get({"offset":%s,"owner_id":%d,"count":100,"filter":"postponed,all"}).items@.text'%(s,-group_id), range(0,300,100)))})
+			'return %s;'%'+'.join(map(lambda s: 'API.wall.get({"offset":%s,"owner_id":%d,"count":100}).items@.text'%(s,-group_id), range(0,300,100)))})
+		self.list_saves += self.vk_user.method('execute',
+			{'code':
+			'return API.wall.get({"owner_id":%d,"filter":"postponed"}).items@.text;'%-group_id})
 		# self.list_saves=self.connection_db.load_list_save()
 		# print(self.list_saves)
 	def load_count_puplic_post(self):
@@ -51,6 +54,7 @@ class Load_Post():
 			{'code':
 			'return API.wall.get({"owner_id":%d,"count":50}).items@.date;'%(-group_id)})
 		# print(self.group_id,'sdddd', list_public_post)
+		
 		self.list_public_post.reverse()
 		return len([i for i in self.list_public_post if int(i)>current_day])
 	
